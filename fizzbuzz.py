336664c34d60d4706e2238c05f7e263cf08b1af7
SATO Daisuke <densuke@fuga.jp>
2025-05-13 03:03:10 +0000

    エラー時に戻り値1で出すようにした

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -27,7 +27,13 @@ def fizzbuzz(n: int) -> str:
 def runtest():
     print("テストを実施します")
     import doctest
-    doctest.testmod()
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("すべてのテストに合格しました")
+    else:
+        print(f"{result.failed}個のテストに失敗しました")
+        import sys
+        sys.exit(1)
 
 
 if __name__ == "__main__":
